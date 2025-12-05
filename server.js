f4c94cd2f7ca9fbb8e1d941d301492fb5d0dc008
Alfonce Yano <ogongoyanojnr@gmail.com>
2025-12-05 10:23:57 +0300

    ...added create note page

--- server.js
+++ server.js
@@ -1,40 +1,32 @@
 const express = require("express");
+const database = require('./database')
 const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"))
+
 app.get("/", (req, res) => {
+  
   res.render("index.ejs");
 });
-const notes = [
-  {
-    id: 1,
-    timestamp: Date.now(),
-    title: "My first Notes",
-    content:
-    "But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
 
-  },
-      
-  {
-    id: 2,
-      timestamp: Date.now(),
-    title: "My second Notes",
-    content:"On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains."
-  },
-];
 app.get("/notes", (req, res) => {
+  const notes =database.getNotes()
   res.render("notes.ejs",{notes});
 });
 
 app.get('/notes/:id',(req,res) => {
   const id = +req.params.id;
-  const note = notes.find(note => note.id === id)
+  const note = database.getNote(id)
   if(!note){
     res.status(404).render("notes404.ejs");
     return
   }
   res.render("note.ejs",{note})
+})
+
+app.get("/createNote",(req,res)=>{
+  res.render("createNote.ejs")
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
